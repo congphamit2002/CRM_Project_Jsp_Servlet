0e68a637ccc37c40616085eba506c89db4e544d7
congphamit2002 <106764102+congphamit2002@users.noreply.github.com>
2022-11-07 23:31:56 +0700

    Update MySQLConnection.java

--- src/main/java/com/java/connection/MySQLConnection.java
+++ src/main/java/com/java/connection/MySQLConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySQLConnection {
-	private final static String url = "jdbc:mysql://127.0.0.1:3307/servlet_crm";
+	private final static String url = "jdbc:mysql://127.0.0.1:3306/servlet_crm";
 	private final static String username = "root";
 	private final static String password = "admin";
 	
